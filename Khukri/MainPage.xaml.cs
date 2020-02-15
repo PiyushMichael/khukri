61498116ac66918dc4ccf23acaecee8c25d481b2
PiyushM <piyush.michael93@gmail.com>
2020-02-15 20:54:47 +0530

    COMPLETED matrix making.

--- Khukri/MainPage.xaml.cs
+++ Khukri/MainPage.xaml.cs
@@ -31,6 +31,9 @@ namespace Khukri
 		private List<String> Articles = new List<String>();
 		private List<List<string>> parsedResult = new List<List<string>>();
 		private List<List<int>> searchMatrix = new List<List<int>>();
+		private List<int> max = new List<int>();
+		private List<int> min = new List<int>();
+		private List<int> avg = new List<int>();
 		private Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
 
 
@@ -43,37 +46,45 @@ namespace Khukri
 
 		async void Run_Click(Object sender, RoutedEventArgs e)
 		{
-			richTextBox.IsEnabled = Run.IsEnabled = plusButton.IsEnabled = minusButton.IsEnabled = false;
-			dropText.Text = "Working...";
-			Loader1.Visibility = Visibility.Visible;
-			urls.Clear();
-			Articles.Clear();
-			foreach (var item in textFields.Children)
+			if (parsedResult.Count != 0)
 			{
-				TextBox x = item as TextBox;
-				if (x.Text != "")
+				richTextBox.IsEnabled = Run.IsEnabled = plusButton.IsEnabled = minusButton.IsEnabled = false;
+				dropText.Text = "Working...";
+				Loader1.Visibility = Visibility.Visible;
+				urls.Clear();
+				Articles.Clear();
+				richTextBox.Document.GetText(Windows.UI.Text.TextGetOptions.None, out string rtb);
+				Articles.Add(rtb.ToLower());
+				foreach (var item in textFields.Children)
 				{
-					urls.Add(x.Text);
+					TextBox x = item as TextBox;
+					if (x.Text != "")
+					{
+						urls.Add(x.Text);
+					}
 				}
-			}
 
-			foreach (var url in urls)
+				foreach (var url in urls)
+				{
+					try
+					{
+						Uri requestUri = new Uri(url);
+						var response = await httpClient.GetAsync(requestUri);
+						var text = await response.Content.ReadAsStringAsync();
+						Articles.Add(Regex.Replace(text.ToLower(), "<.*?>", ""));
+					}
+					catch (Exception) { }
+				}
+
+				richTextBox.IsEnabled = Run.IsEnabled = plusButton.IsEnabled = minusButton.IsEnabled = true;
+				dropText.Text = "Drop files here...";
+				Loader1.Visibility = Visibility.Collapsed;
+				KeywordAnalysis();
+			} else
 			{
-				try
-				{
-					Uri requestUri = new Uri(url);
-					var response = await httpClient.GetAsync(requestUri);
-					var text = await response.Content.ReadAsStringAsync();
-					Articles.Add(text.ToLower());
-				}
-				catch (Exception) {}
+				var messageDialog = new Windows.UI.Popups.MessageDialog("Add csv file of keywords first");
+				await messageDialog.ShowAsync();
 			}
-
-			richTextBox.IsEnabled = Run.IsEnabled = plusButton.IsEnabled = minusButton.IsEnabled = true;
-			dropText.Text = "Drop files here...";
-			Loader1.Visibility = Visibility.Collapsed;
-			//dragBox.Text = urls.Count.ToString() + ' ' + Articles.Count.ToString();
-			KeywordAnalysis();
 		}
 
 		void Handle_Input(Object sender, RoutedEventArgs e)
@@ -97,7 +108,6 @@ namespace Khukri
 			foreach (var record in records)
 			{
 				var fields = record.Split(',');
-				// dragBox.Text += fields.Length.ToString() + '\n';
 				var recordItem = new List<string>();
 				int last;
 				foreach (var field in fields)
@@ -126,6 +136,10 @@ namespace Khukri
 		void KeywordAnalysis()
 		{
 			searchMatrix.Clear();
+			max.Clear();
+			min.Clear();
+			avg.Clear();
+
 			foreach (var entry in parsedResult)
 			{
 				var keyword = entry[0];
@@ -135,12 +149,16 @@ namespace Khukri
 				List<int> counts = new List<int>();
 				foreach (var article in Articles)
 				{
-					dragBox.Text += keyword + ": " + Regex.Matches(article, keyword).Count.ToString() + '\n';
 					counts.Add(Regex.Matches(article, keyword).Count);
 				}
 				searchMatrix.Add(counts);
-				//var line = keyword + " | " + maxSearches + " | " + competition + " | " + bidding + '\n';
-				//dragBox.Text += line;
+			}
+
+			foreach (var searchRow in searchMatrix)
+			{
+				max.Add(searchRow.Max());
+				min.Add(searchRow.Min());
+				avg.Add(Convert.ToInt32(searchRow.Average()));
 			}
 		}
 
@@ -160,8 +178,13 @@ namespace Khukri
 							var str = new Windows.Storage.Streams.Buffer(length);
 							await x.ReadAsync(str, length, Windows.Storage.Streams.InputStreamOptions.ReadAhead);
 							var dataReader = Windows.Storage.Streams.DataReader.FromBuffer(str);
-							var output = dataReader.ReadString(str.Length);
-							GenerateKeywords(output, parsedResult);
+							try {
+								var output = dataReader.ReadString(str.Length);
+								GenerateKeywords(output, parsedResult);
+							} catch(Exception) {
+								var messageDialog = new Windows.UI.Popups.MessageDialog("Invalid csv file. Save file as CSV (MS-DOS) (*.csv) format.");
+								await messageDialog.ShowAsync();
+							}
 						} else {
 							var messageDialog = new Windows.UI.Popups.MessageDialog("Invalid file type. Only drop .csv files.");
 							await messageDialog.ShowAsync();
